5883fac63ab66b082465472d4650d593b8c8b7dc
지수영 <jisooyoung97@gmail.com>
2024-06-13 23:32:31 +0900

    Feat: octokit으로Github API 사용하여 commit 불러오기

--- src/services/connectRepository.ts
+++ src/services/connectRepository.ts
@@ -1,7 +1,25 @@
 import { pool } from "../utils/db";
+import { Endpoints } from "@octokit/types";
+import { octokit } from "../api/octokit";
+
+async function getAllCommits(
+    owner: string,
+    repo: string,
+): Promise<Endpoints['GET /repos/{owner}/{repo}/commits']['response']['data']> {
+    const result = await octokit.repos.listCommits({
+        owner,
+        repo,
+        per_page: 100,
+    });
+    console.log(`API response status: ${result.status}`);
+    console.log(result.data);
+    return result.data;
+}
+
+console.log(getAllCommits('ssafy-11th-seoul10', '2day-1algo'));
 
 function test() {
-    
+
     const str: string = "connectRepository";
 
     return str;
