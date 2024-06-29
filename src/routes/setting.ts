247e5d7910e99b04d0538f0c81f90d2758eae02f
sw0501 <dkxkqkrtkddn@naver.com>
2024-06-29 19:18:29 +0900

    Feat: Setting CRUD 湲곕뒫 援ы쁽

--- src/routes/setting.ts
+++ src/routes/setting.ts
@@ -2,69 +2,175 @@ import express, { Request, Response, Router } from "express";
 import * as setting from '../services/setting';
 import { DBInfo } from "../@types/db.interface";
 
-
 const router: Router = express.Router();
 
 //Get Setting Infos
 router.get("/", async (req: Request, res: Response) => {
 
-    const infoList = setting.getInfoList();
+    try {
+        const ownerId:string = req.params.owner_id;
+        const repoName:string = req.params.repo_name;
+    
+        const settingInfo = await setting.getInfo(ownerId, repoName);
+    
+        if (settingInfo) {
+            return res.status(200).json({
+                info_list: settingInfo
+            });        
+        }
+        else {
+            throw new Error("존재하지 않는 정보입니다.");    
+        }
 
-    return res.status(200).json({
-        info_list: infoList
-    });
-});
+    } catch (error) {
+        console.error(error);
 
-//Get Setting Info
-router.get("/:id", async (req: Request, res: Response) => {
-
-    const info = setting.getInfo(req.params.id);
-
-    return res.status(200).json({
-        info: info
-    });
+        return res.status(500).json({
+            message: "Failed to get setting info"
+        });
+    }
+    
 });
 
 //Add New Setting
 router.post("/", async (req: Request, res: Response) => {
 
-    const dbInfo: DBInfo = {
-        repo: {
-            id: req.body.repo.id,
-            name: req.body.repo.name
-        },
-        owner: {
-            id: req.body.owner.id,
-            name: req.body.owner.name,
-            github_access_token: req.body.owner.github_access_token
-        },
-        webhook: {
-            server: "",
-            discord: req.body.webhook.discord,
-            slack: req.body.owner.slack,
-            mattermost: req.body.owner.mattermost
-        },
-        schedule: [
-            req.body.schedule
-        ]
-    }
-    
-    return res.status(200).json({
+    try {
+        const dbInfo: DBInfo = {
+            repo: {
+                id: req.body.repo.id,
+                name: req.body.repo.name
+            },
+            owner: {
+                id: req.body.owner.id,
+                name: req.body.owner.name,
+                github_access_token: req.body.owner.github_access_token
+            },
+            webhook: {
+                server: "",
+                discord: req.body.webhook.discord,
+                slack: req.body.owner.slack,
+                mattermost: req.body.owner.mattermost
+            },
+            schedule: [
+                req.body.schedule
+            ]
+        }
+        
+        const settingInfo = await setting.saveInfo(dbInfo);
 
-    });
+        if (settingInfo) {
+            return res.status(200).json({
+                message: "Setting added successfully"
+            });     
+        }
+        else {
+            throw new Error("정보 저장에 실패했습니다.");    
+        }
+
+        
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Failed to save setting Info",
+            error: error
+        })
+    }
+
+    
 });
 
 //Modify Setting
 router.put("/", async (req: Request, res: Response) => {
-    return res.status(200).json({
-    });
+    try {
+        const dbInfo: DBInfo = {
+            repo: {
+                id: req.body.repo.id,
+                name: req.body.repo.name
+            },
+            owner: {
+                id: req.body.owner.id,
+                name: req.body.owner.name,
+                github_access_token: req.body.owner.github_access_token
+            },
+            webhook: {
+                server: "",
+                discord: req.body.webhook.discord,
+                slack: req.body.owner.slack,
+                mattermost: req.body.owner.mattermost
+            },
+            schedule: [
+                req.body.schedule
+            ]
+        }
+        
+        const settingInfo = await setting.saveInfo(dbInfo);
+
+        if (settingInfo) {
+            return res.status(200).json({
+                message: "Success to Setting Update"
+            });
+        }
+        else {
+            throw new Error("정보 수정에 실패했습니다.");    
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Failed to Setting Update",
+            error: error
+        });
+    }
 });
   
 
 //Delete Setting
 router.delete("/", async (req: Request, res: Response) => {
-    return res.status(200).json({
-    });
+    try {
+
+
+        const dbInfo: DBInfo = {
+            repo: {
+                id: req.body.repo.id,
+                name: req.body.repo.name
+            },
+            owner: {
+                id: req.body.owner.id,
+                name: req.body.owner.name,
+                github_access_token: req.body.owner.github_access_token
+            },
+            webhook: {
+                server: "",
+                discord: req.body.webhook.discord,
+                slack: req.body.owner.slack,
+                mattermost: req.body.owner.mattermost
+            },
+            schedule: [
+                req.body.schedule
+            ]
+        }
+
+        const settingInfo = await setting.deleteInfo(dbInfo);
+
+        if (settingInfo) {
+            return res.status(200).json({
+                message: "Success to Setting deleted",
+            });
+        }
+        else {
+            throw new Error("정보 삭제에 실패했습니다.");    
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Failed to Setting deleted",
+            error: error
+        });
+    }
+
   });
 
 export = router;
--- src/services/setting.ts
+++ src/services/setting.ts
@@ -1,23 +1,73 @@
-import axios, { AxiosResponse } from 'axios';
-
+import { DBInfo } from "../@types/db.interface";
 import Info from '../models/info';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-async function getInfoList() {
-    const infoList = await Info.find({});
+async function saveInfo(dbInfo: DBInfo) {
+    try {
+        const newInfo = new Info(dbInfo);
+        await newInfo.save();
+        return newInfo;    
+    }
+    catch (error) {
+        throw error;
+    }
     
-    return infoList;
 }
 
-async function getInfo(id:string) {
-    const info = await Info.findOne({ id: id });
-    
-    return info;
+async function getInfo(ownerId: string, repoName: string) {
+    try {
+        const settingInfo = await Info.findOne(
+            {
+                owner: {
+                    id: ownerId
+                },
+                repo: {
+                    name: repoName
+                }
+            }
+        );
+        
+        return settingInfo;    
+    }
+    catch (error) {
+        throw error;
+    }
 }
 
+async function modifyInfo(dbInfo: DBInfo) {
+    try {
+     
+        const settingInfo = await Info.findOneAndUpdate({
+            owenr: {
+                id: dbInfo.owner.id
+            }, repo: {
+                name: dbInfo.repo.name
+            }
+        }, dbInfo, { new: true, runValidators: true });
+        
+        return settingInfo;   
+    } catch (error) {
+        throw error;
+    }
+}
 
+async function deleteInfo(dbInfo: DBInfo) {
+    try {
+        const settingInfo = await Info.findOneAndDelete({
+            owenr: {
+                id: dbInfo.owner.id
+            }, repo: {
+                name: dbInfo.repo.name
+            }
+        });
+    
+        return settingInfo;    
+    }
+    catch (error) {
+        throw error;
+    }
+}
 
-export { getInfoList,getInfo };
+export { saveInfo, getInfo,modifyInfo, deleteInfo};
