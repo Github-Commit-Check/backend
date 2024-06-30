b5b59bf223e21af01927ce7c21b67903e9807b79
sw0501 <dkxkqkrtkddn@naver.com>
2024-06-30 20:13:57 +0900

    Remove: 테스트 코드 제거

--- src/services/connectRepository.ts
+++ src/services/connectRepository.ts
@@ -76,15 +76,4 @@ async function listCommits() {
   );
 }
 
-function test() {
-  const str: string = "connectRepository";
-
-  return str;
-}
-
-async function dbTest() {
-  const [rows, fields] = await pool.query("SELECT 1");
-  return rows;
-}
-
-export { test, dbTest, listCommits };
+export { listCommits };
